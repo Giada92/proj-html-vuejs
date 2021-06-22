de08cfe2bb068c63ba80e7f084269ccc3481ac7b
Giada92 <giada_meloni@virgilio.it>
2021-06-22 11:47:31 +0200

    Finita terza sezione main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -36,6 +36,30 @@
         </div>
       </section>
       <!-- /Sezione Service -->
+
+      <!-- Sezione Our Work -->
+      <section id="work">
+        <div class="container">
+            <h2 class="text-center fw-bold">Our Work</h2>
+            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, qui.</p>
+            <div class="d-flex flex-wrap justify-content-center">
+              <!-- Contenitore gallery che cicla le foto-->
+              <div class="wrapper_gallery" v-for="el, index in gallery" :key="index">
+                <div class="gallery_img">
+                  <img :src="el.url" alt="">
+                  <div class="layover">
+                    <i class="fas fa-link"></i>
+                  </div>
+                </div>
+              </div>
+              <!-- /Contenitore gallery che cicla le foto-->
+            </div>
+            <div class="text-center mt-5">
+              <span class="btn_orange">View our work </span>
+            </div>
+        </div>
+      </section>
+      <!-- /Sezione Our Work -->
   </main>
 </template>
 
@@ -47,14 +71,40 @@ export default {
     components:{
       Card
     },
+    data(){
+      return{
+        gallery:[
+          {
+            url: require("../assets/img/case-study-gallery-3-1-800x600.jpg")
+          },
+          {
+            url: require("../assets/img/case-study-gallery-2-800x600.jpg")
+          },
+          {
+            url: require("../assets/img/case-study-gallery-1-1-800x600.jpg")
+          },
+          {
+            url: require("../assets/img/case-study-gallery-4-1-800x600.jpg")
+          },
+          {
+            url: require("../assets/img/case-study-gallery-5-1-800x600.jpg")
+          },
+          {
+            url: require("../assets/img/case-study-gallery-6-1-800x600.jpg")
+          }
+        ]
+      }
+    },
     props:{
       newTemplate: Array,
-      services:Array
+      services:Array,
+      work: Array
     }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/style/globali.scss";
 
 /* Sezione new Template */
 #new_template {
@@ -75,8 +125,7 @@ export default {
 
 /* Sezione Service */
 #service {
-  height: 700px;
-  padding-top: 100px;
+  padding: 100px 0;
   background-image: url('../assets/img/pattern_background.png');
   background-size: contain;
   background-color: #051642;
@@ -90,4 +139,69 @@ export default {
 }
 /* Sezione Service */
 
+/* Sezione Work */
+
+#work {
+  width: 100%;
+  padding: 100px 0;
+}
+
+.wrapper_gallery {
+  width: calc(100% / 3 );
+  margin: 10px 0;
+}
+
+.gallery_img:hover > .layover {
+    visibility: visible;
+}
+
+.gallery_img {
+    position: relative;
+}
+
+.gallery_img{
+    img {
+    display: block;
+    max-width: 95%;
+    object-fit: cover;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+
+.layover {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 95%;
+    height: 100%;
+    position: absolute;
+    top:0;
+    left: 0;
+    background-color: rgba(#0369d7, 0.5);
+    cursor: pointer;
+    color: white;
+    visibility: hidden;
+}
+
+.layover.circle {
+    border-radius: 50%;
+}
+.layover > i {
+    font-size: 40px;
+}
+
+.btn_orange {
+    @include btn_orange;
+    padding: 15px 25px;
+    margin-right: 15px;
+    color: white;
+
+    &:hover{
+        background-color: $orange-light;
+    }
+}
+
+/* Sezione Work */
 </style>
